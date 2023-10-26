fff22c10f035043a8fc6d2d4984760c7521a5e90
Isaac Song <isaacsongs10@gmail.com>
2023-10-25 20:14:14 -0700

    anagram lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagram_list):
+        match_list = []
+        for word in anagram_list:
+            count = 0
+            for letter in self.word:
+                if letter in word:
+                    count += 1
+                else:
+                    continue
+            if count == len(self.word) == len(word):
+                match_list.append(word)
+        return match_list if len(match_list) else []
+
+listen = Anagram("Word")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
